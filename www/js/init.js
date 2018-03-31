3dbe40f6cbeebf728d32c2d22ad962fc1c058bfa
Lera Kulikova <eklera0506@gmail.com>
2018-03-31 03:39:00 +0300

    initial commit

---
+++ www/js/init.js
@@ -0,0 +1,13 @@
+$( function() {
+// Init tabs
+$( "#tabs" ).tabs();
+$( "#tabs-portfolio" ).tabs();
+
+// init slick carousel
+$('.reviews-carousel').slick({
+    dots: true,
+    prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+    nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
+  });
+  
+});
