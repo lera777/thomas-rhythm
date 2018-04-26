8e4d8e69ca3556dd65a39dcf270c099265804826
Lera Kulikova <eklera0506@gmail.com>
2018-04-26 06:49:57 +0300

    readme

--- www/js/init.js
+++ www/js/init.js
@@ -9,5 +9,10 @@ $('.reviews-carousel').slick({
     prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
     nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
   });
-  
+
+  // init slick nav
+  $('.header-nav').slicknav({
+    appendTo: '.header .container-fluid',
+    label: ''
+  });
 });
